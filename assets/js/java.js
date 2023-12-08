3406d63c5c05d1de19f0074e92f0d5de3e8be53e
Josh484 <josh.m.mendoza1@gmail.com>
2023-12-08 20:27:12 +0000

    added button to get value and save to storage

--- assets/js/java.js
+++ assets/js/java.js
@@ -5,20 +5,52 @@ $(function(){
     var getDay = dayjs();
     textCDay.text(getDay.format("dddd, MMMM  Do"));
 
-    var tableTime = [9,10,11,12,13,14,15,16,17];
+    var tableTime = [9,10,11,12,13,14,15,16,19];
     var startofHour = dayjs().startOf('hour');
+
+    function getLocal() {
+        
+    }
+    
     for (var i = 0; i < tableTime.length; i++){
+        var createNewTable = $("<tr></tr>");
+        var tableTimeGetter = $('<td></td>');
+        var tableContent = $('<td></td>');
+        var inputContent = $('<input></input>');
+        var tableSaver = $('<td></td>');
+        var button = $('<button type="button" class="btn btn-dark">Dark</button>')
+
+
+        tableTimeGetter.text(tableTime[i]);
+        tableContent.append(inputContent)
+        tableSaver.append(button);
+        $('#putContent').append(createNewTable);
+        createNewTable.append(tableTimeGetter);
+        createNewTable.append(tableContent);
+        createNewTable.append(tableSaver);
+        button.attr('data-submit', + tableTime[i]);
+        inputContent.attr('data-content', + tableTime[i])
+        
         if(tableTime[i] > startofHour.format('HH')) {
-            
+            tableContent.attr('id', 'future');
         }
         else if (tableTime[i] < startofHour.format('HH')){
-
+            tableContent.attr('id', 'past');
+            
         } else {
-
+            tableContent.attr('id', 'present');
+            
         }
         
         
     }
+    $('#putContent').delegate('.btn','click', function(event){
+        event.preventDefault();
+        var clickedItem = $(event.target);
+        var getData = clickedItem.attr('data-submit');
+        localStorage.setItem(getData, clickedItem.parent().parent().children().eq(1).children().val());
+    });
+
     console.log()
     
 });

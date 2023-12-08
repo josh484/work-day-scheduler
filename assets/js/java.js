9336d587f3d1d3b6bbe3ab360e45a546ba3765e1
Josh484 <josh.m.mendoza1@gmail.com>
2023-12-08 18:58:16 +0000

    created an array to store times between 9am-17pm a for loop for the array and if statments for the number within the array to be greater, lower or equal to the current hour

--- assets/js/java.js
+++ assets/js/java.js
@@ -4,4 +4,21 @@ $(function(){
     dayjs.extend(window.dayjs_plugin_advancedFormat);
     var getDay = dayjs();
     textCDay.text(getDay.format("dddd, MMMM  Do"));
+
+    var tableTime = [9,10,11,12,13,14,15,16,17];
+    var startofHour = dayjs().startOf('hour');
+    for (var i = 0; i < tableTime.length; i++){
+        if(tableTime[i] > startofHour.format('HH')) {
+            
+        }
+        else if (tableTime[i] < startofHour.format('HH')){
+
+        } else {
+
+        }
+        
+        
+    }
+    console.log()
+    
 });
